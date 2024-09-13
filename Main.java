665a2256c88f9138a51dfdd7e78b7645976c3443
Nico11c <nickky123654@gmail.com>
2024-09-12 22:55:14 -0600

    eliminacion de archivo

---
+++ Main.java
@@ -0,0 +1,66 @@
+import java.sql.SQLOutput;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //esta es un vairbale de tipo estring
+        String nombre = "Nicolas";
+
+        //esta es una variable de tipo int
+        int edad=22;
+
+        //varible de tipo cararter
+        char genero = 'h';
+
+        //variable de tipo decimal
+        double estatura= 1.75;
+
+        boolean esCasado= false;
+
+
+
+
+
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(genero);
+        System.out.println( estatura);
+        System.out.println(esCasado);
+
+        System.out.println("mi nombre es " + nombre + " y mi edad es : " + edad + " mi genero es :" + genero + " mi altura es :" + estatura + " soy casado  : "+ esCasado  );
+
+
+        //SECCION UNO
+        System.out.println("el mobre de mi hermana andrea y tiene 24 ans mide 1.65 mts nacio en el 2002 es mujer");
+
+
+        //seccion dos
+        String nombreDeMiMascota = " pambazo ";
+        String raza = " gold fish";
+        String color = " calico";
+        System.out.println(" el nombre de mi mascota es " + nombreDeMiMascota  + " es un " + raza + " y es de color " + color );
+
+
+
+        //seccion tres
+        String nombrePelicula = "piratas del caribe";
+        int anDeLazanamineto = 2001;
+        double duracion = 2.5;
+        String generoP = " accion y aventura " ;
+        String director= "yo merengues";
+        System.out.println("mi pelicula favorita es : " + nombrePelicula+ " se lanzo en el ans " + anDeLazanamineto + " el genero es  " + generoP + " el director es " + director );
+        System.out.println();
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- Tarea.py
+++
@@ -1,5 +0,0 @@
-print (hola mundo)
-print (actualizacion )
-print (actualizacion tres)
-int num=2
-num 3
